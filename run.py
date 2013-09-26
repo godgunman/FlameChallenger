83d01c04c92776ee8b3e1df53afc6dac1b012299
godgunman <godgunman@gmail.com>
2013-09-27 00:10:42 +0800

    refactoring by creating a PumpSpark class.

--- run.py
+++ run.py
@@ -5,10 +5,31 @@
 import serial
 
 port = '/dev/tty.usbserial-DAWR13BI'
-ser = serial.Serial(port, 9600, timeout=1)
 
-# start
-ser.write(bytearray([255,0,254]))
+class PumpSpark:
 
-# stop
-ser.write(bytearray([255,0,0]))
+  ser = None
+  def connect(self):
+    try:
+      self.ser = serial.Serial(port, 9600, timeout=1)
+      print 'connected serial port.'
+    except: 
+      print 'connected fail.'
+      pass
+
+  def turnOff(self, *args):
+    if len(args) == 0:
+      for i in range(0, 8):
+        self.ser.write(bytearray([255,i,0]))
+    else:
+      for i in args:
+        self.ser.write(bytearray([255,i,0]))
+
+  def turnOn(self, p, *args):
+    if len(args) == 0:
+      for i in range(0, 8):
+        self.ser.write(bytearray([255,i,p]))
+    else:
+      for i in args:
+        self.ser.write(bytearray([255,i,p]))
+
